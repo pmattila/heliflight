e1498365c5bc432a5b1c7588e072e221c740061d
Petri Mattila <petri.t.j.mattila@gmail.com>
2020-09-17 15:07:09 +0100

    WIP: Add STM32H743 target

---
+++ src/main/target/STM32H743/target.mk
@@ -0,0 +1,33 @@
+#
+# This file is part of Heliflight 3D.
+#
+# Heliflight 3D is free software. You can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Heliflight 3D is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software. If not, see <https://www.gnu.org/licenses/>.
+#
+
+H743xI_TARGETS += $(TARGET)
+
+#CUSTOM_DEFAULTS_EXTENDED = yes
+
+HSE_VALUE   = 25000000
+
+FEATURES    += VCP SDCARD_SDIO ONBOARDFLASH
+
+TARGET_SRC = \
+        $(addprefix drivers/accgyro/,$(notdir $(wildcard $(SRC_DIR)/drivers/accgyro/*.c))) \
+        $(addprefix drivers/barometer/,$(notdir $(wildcard $(SRC_DIR)/drivers/barometer/*.c))) \
+        $(addprefix drivers/compass/,$(notdir $(wildcard $(SRC_DIR)/drivers/compass/*.c))) \
+
+#        drivers/bus_quadspi_hal.c \
+#        drivers/bus_quadspi.c \
+
